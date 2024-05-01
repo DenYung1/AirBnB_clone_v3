29f3ce985d483a77d2d5fa1e71f7a09fcee79555
DenYung1 <dennisabayateye@yahoo.com>
2024-04-30 19:56:48 -0700

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-"""This module creates a Amenity class"""
+"""
+Amenity Class from Models Module
+"""
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import backref
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
-from models.base_model import BaseModel
 
-
-class Amenity(BaseModel):
-    """Class for managing amenity objects"""
-
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class handles all application amenities"""
+    if storage_type == "db":
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity")
+    else:
+        name = ''
