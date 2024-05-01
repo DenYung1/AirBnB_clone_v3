993cf6f82b94456a39365c765fbab0d9279499b0
DenYung1 <dennisabayateye@yahoo.com>
2024-04-30 19:58:49 -0700

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,11 +1,39 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
+"""
+City Class from Models Module
+"""
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+import models
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
-from models.base_model import BaseModel
 
+class City(BaseModel, Base):
+    """City class handles all application cities"""
+    if storage_type == "db":
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities', cascade='delete')
+    else:
+        state_id = ''
+        name = ''
 
-class City(BaseModel):
-    """Class for managing city objects"""
+    if storage_type != 'db':
+        @property
+        def places(self):
+            """
+            getter for places
+            :return: list of places in that city
+            """
+            all_places = models.storage.all("Place")
 
-    state_id = ""
-    name = ""
+            result = []
+
+            for obj in all_places.values():
+                if str(obj.city_id) == str(self.id):
+                    result.append(obj)
+
+            return result
